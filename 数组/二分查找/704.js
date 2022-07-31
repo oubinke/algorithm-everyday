ee2c3aba0c987de14defd5cc23206ec653c0b0fd
binkeou <binkeou@tencent.com>
2022-07-31 21:39:29 +0800

    二分查找

--- 数组/二分查找/704.js
+++ 数组/二分查找/704.js
@@ -1,10 +1,28 @@
-const seach = function(num, target) {
+const seach1 = function(num, target) {
   let left = 0;
   let right = num.length - 1;
   while (left <= right) {
-    const mid = Math.floor((right + left) / 2);
+    // 直接使用left+right可能会溢出
+    const mid = left + Math.floor((right - left) / 2);
     if (num[mid] > target) {
       right = mid - 1
+    } else if (num[mid] < target) {
+      left = mid + 1
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+const seach2 = function(num, target) {
+  let left = 0;
+  let right = num.length;
+  while (left < right) {
+    // 直接使用left+right可能会溢出
+    const mid = left + Math.floor((right - left) / 2);
+    if (num[mid] > target) {
+      right = mid
     } else if (num[mid] < target) {
       left = mid + 1
     } else {
